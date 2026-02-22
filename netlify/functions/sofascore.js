74c3575f0b3603e8564920e0e3483e8b3e95f3c8
Ahmed Hazem Zedan MAHMOD (210501265) <210501265@ogr.altinbas.edu.tr>
2026-02-22 15:38:23 +0300

    fix async function

--- netlify/functions/sofascore.js
+++ netlify/functions/sofascore.js
@@ -1,17 +1,26 @@
-const response = await fetch(
-  "https://sofascore.p.rapidapi.com/tournaments/get-next-matches?tournamentId=178&seasonId=29415&pageIndex=0",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": process.env.RAPIDAPI_KEY,
-      "x-rapidapi-host": "sofascore.p.rapidapi.com"
-    }
+exports.handler = async function (event, context) {
+  try {
+    const response = await fetch(
+      "https://sofascore.p.rapidapi.com/tournaments/get-next-matches?tournamentId=178&seasonId=29415&pageIndex=0",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+          "x-rapidapi-host": "sofascore.p.rapidapi.com"
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
   }
-);
-
-const data = await response.json();
-
-return {
-  statusCode: 200,
-  body: JSON.stringify(data),
 };
